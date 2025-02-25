1fa36ee0e06002ec8469a2cbdbb3297647633b88
malopez19 <lopezmateojurado@gmail.com>
2025-02-25 18:39:32 -0500

    add Modal component with context management and integrate into App

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,20 @@ import Button, { ColorRed } from "./components/Button/Button";
 import Customform from "./components/CustomForm/Customform";
 import { GlobalProvider } from "./context/global.provider";
 import { BookReader } from "./components/useRef/BookReader";
+import { Modal } from "./components/Modal/Modal";
+import { useModalContext } from "./components/Modal/context/useModalContext";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const { setState} = useModalContext();
 
 	const handleclick = () => {
 		console.log("Button clicked");
 		setCount(count + 1);
+	};
+
+	const openModal = () => {
+		setState(true);
 	};
 
 	return (
@@ -27,6 +34,16 @@ function App() {
 			<br></br>
 			<br></br>
 			<BookReader />
+
+			<br></br>
+			<br></br>
+			<Modal>
+				<div>
+					<h1>Modal</h1>
+					<p>Modal content</p>
+				</div>
+			</Modal>
+			<button onClick={openModal}>Click me</button>
 		</GlobalProvider>
 	);
 }
